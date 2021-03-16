a9c9aeb1c58345046ad1e5f6b4648ec51a6672ad
rbg <rambabugovind@hotmail.com>
2021-03-16 14:41:25 +0530

    initial setup

---
+++ readme.md
@@ -0,0 +1,4 @@
+Trying out a react app from scratch based on the following tutorials:
+
+1. https://github.com/yakkomajuri/react-from-scratch#readme
+2. https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, // matches .js and .jsx files
+                loader: 'babel-loader', // uses babel-loader for the specified file types (no ts-loader needed)
+                exclude: /node_modules/, 
+            },
+            {
+                test: /\.css$/, // matches .css files only (i.e. not .scss, etc)
+                use: ['style-loader', 'css-loader'], 
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.jsx', '.js']
+    },
+    output: {
+        filename: 'bundle.js' // output bundle
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http://localhost:3000/dist/',
+        hotOnly: true,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()], // used for hot reloading when developing
+    devtool: 'eval-source-map', // builds high quality source maps
+};
